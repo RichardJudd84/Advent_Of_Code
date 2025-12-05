a2e1627c5b8d027fc93454c6f9f9387346b830cb
Richard Judd <richard_judd84@hotmail.com>
2025-12-05 12:59:59 +0100

    day 5

--- AOC_25/day_5/solution_1.py
+++ AOC_25/day_5/solution_1.py
@@ -1,5 +1,6 @@
 import sys
-import os  
+import os 
+from typing import List
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from utils.helpers import *
@@ -8,5 +9,39 @@ import input_paths
 testData = getLineStrings(input_paths.input1ExamplePath)
 data = getLineStrings(input_paths.input1Path)
 
+def getIngedientsAndRanges(lines: List[str]):
+    ranges: List[tuple[str]] = []
+    ingredients: List[str] = []
+
+    for line in lines:
+        if('-' in line):
+            ranges.append(tuple(line.split('-')))
+        elif(line.isdigit()):
+            ingredients.append(line)
+    
+    return ranges, ingredients
+
+def isValueInRange(range: tuple[str], value: str):
+    start, end = range
+    isInRange =  int(value) >= int(start) and int(value) <= int(end)
+    return isInRange
+
+def isValueInRanges(ranges: List[tuple[str]], value:str):
+    for range in ranges:
+        isInRange = isValueInRange(range, value)
+        if(isInRange):
+            return True
+        
+    return False
+
+
 if(__name__ == '__main__'):
-    pass
+    ranges, ingredients = getIngedientsAndRanges(data)
+
+    count = 0
+
+    for item in ingredients:
+       if(isValueInRanges(ranges, item)):
+          count += 1
+
+    print(count)
--- AOC_25/day_5/solution_2.py
+++ AOC_25/day_5/solution_2.py
@@ -1,5 +1,6 @@
 import sys
-import os  
+import os
+from typing import List
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils.helpers import *
 import input_paths
@@ -8,7 +9,49 @@ from solution_1 import *
 testData = getLineStrings(input_paths.input1ExamplePath)
 data = getLineStrings(input_paths.input1Path)
 
+def mergeAllRanges(ranges: List[tuple[str]]):
+    ranges = sortRanges(ranges)
+    previous = ranges[0]
+    mergedRanges: List[tuple[str]] = []
+
+    for i in range(1,len(ranges)):
+        current = ranges[i]
+        previousEnd = int(previous[1])
+        currentStart = int(current[0])
+        if( currentStart <= previousEnd + 1 ):
+            previous = mergeRanges(previous, current)
+        else: 
+            mergedRanges.append(previous)
+            previous = current
+
+    mergedRanges.append(previous)
+    return mergedRanges
+    
+
+def mergeRanges(range1: tuple[str], range2: tuple[str]):
+    start1, end1 = range1 
+    start2, end2 = range2 
+    return (min(start1,start2), max(end1,end2))
+
+def sortRanges(ranges: List[tuple[str]]):
+    ranges.sort(key=lambda x: int(x[0]))
+    return ranges
+
+def getNumberInRange(range: tuple[str]):
+    return int(range[1]) - int(range[0]) + 1
+
+
 
 if(__name__ == '__main__'):
-    pass
+    ranges, ingredients = getIngedientsAndRanges(data)
+    mergedRanges = mergeAllRanges(ranges)
 
+    sum = 0
+
+    for r in mergedRanges:
+        sum += getNumberInRange(r)
+
+    print(sum)
+
+
+# 336185947296474 to Low
